7ec2a54da539150c3489a930296808e55f4c9924
shunyue1320 <tsunyue1320@gmail.com>
2023-08-06 17:09:52 +0800

    chore: 根目录 vite 配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineApplicationConfig } from '@vben/vite-config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineApplicationConfig({
+  overrides: {}
 })
