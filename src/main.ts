0fef5f84e33a94ac2788171fff67a170fff9f400
shunyue1320 <tsunyue1320@gmail.com>
2023-08-06 20:57:20 +0800

    feat: 初始化时注册全局组件

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 
-import App from './App.vue';
-import { setupStore } from '@/store';
+import App from './App.vue'
+import { setupStore } from '@/store'
+import { registerGlobComp } from '@/components/registerGlobComp'
 
 async function bootstrap() {
-  const app = createApp(App);
+  const app = createApp(App)
 
   // 配置存储
-  setupStore(app);
+  setupStore(app)
+
+  // // 初始化内部系统配置
+  // initAppConfigStore();
+
+  // 注册全局组件
+  registerGlobComp(app)
 }
 
-bootstrap();
+bootstrap()
