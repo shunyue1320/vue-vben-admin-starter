d1265e15558da6cfee03b725f282ec183aea6315
shunyue1320 <tsunyue1320@gmail.com>
2023-08-07 21:09:28 +0800

    feat: 根路径重定向到dashboard

---
+++ src/enums/pageEnum.ts
@@ -0,0 +1,10 @@
+export enum PageEnum {
+  // 基本登录路径
+  BASE_LOGIN = '/login',
+  // 基本主页路径
+  BASE_HOME = '/dashboard',
+  // 基本错误路径
+  ERROR_PAGE = '/exception',
+  // 错误日志页路径
+  ERROR_LOG_PAGE = '/error-log/list',
+}
--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,6 +1,7 @@
 import { AppRouteRecordRaw } from '@/router/types'
 
 import { t } from '@/hooks/web/useI18n'
+import { PageEnum } from '@/enums/pageEnum'
 
 export const LoginRoute: AppRouteRecordRaw = {
   path: '/login',
@@ -11,10 +12,20 @@ export const LoginRoute: AppRouteRecordRaw = {
   },
 }
 
+// 根路由
+export const RootRoute: AppRouteRecordRaw = {
+  path: '/',
+  name: 'Root',
+  redirect: PageEnum.BASE_HOME,
+  meta: {
+    title: 'Root',
+  },
+}
+
 // 未经许可的基本路由
 export const basicRoutes = [
   LoginRoute,
-  // RootRoute,
+  RootRoute,
   // REDIRECT_ROUTE, // 重定向路由
   // PAGE_NOT_FOUND_ROUTE, // 404页面
 ]
