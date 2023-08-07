5ee796f49cca5d189a64b5964cec33ad8923f7f1
shunyue1320 <tsunyue1320@gmail.com>
2023-08-07 17:02:25 +0800

    feat: 修改弱色模式和灰色模式状态相关代码

---
+++ src/logics/theme/updateColorWeak.ts
@@ -0,0 +1,9 @@
+import { toggleClass } from './util'
+
+/**
+ * 更改项目弱色模式的状态
+ * @param colorWeak
+ */
+export function updateColorWeak(colorWeak: boolean) {
+  toggleClass(colorWeak, 'color-weak', document.documentElement)
+}
