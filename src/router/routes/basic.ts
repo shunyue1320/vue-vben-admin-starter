e87e4b609e7a25ce9f5729ca36bc13497d45f551
shunyue1320 <tsunyue1320@gmail.com>
2023-08-08 22:25:42 +0800

    feat: 添加错误日志页面

---
+++ src/router/routes/basic.ts
@@ -0,0 +1,27 @@
+import type { AppRouteRecordRaw } from '../types'
+
+import { t } from '@/hooks/web/useI18n'
+
+export const ERROR_LOG_ROUTE: AppRouteRecordRaw = {
+  path: '/error-log',
+  name: 'ErrorLog',
+  component: LAYOUT,
+  redirect: '/error-log/list',
+  meta: {
+    title: 'ErrorLog',
+    hideBreadcrumb: true, // 隐藏面包屑
+    hideChildrenInMenu: true, // 隐藏子菜单
+  },
+  children: [
+    {
+      path: 'list',
+      name: 'ErrorLogList',
+      component: () => import('@/views/sys/error-log/index.vue'),
+      meta: {
+        title: t('routes.basic.errorLogList'),
+        hideBreadcrumb: true,
+        currentActiveMenu: '/error-log',
+      },
+    },
+  ],
+}
---
+++ src/views/sys/error-log/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>错误日志页面</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less"></style>
