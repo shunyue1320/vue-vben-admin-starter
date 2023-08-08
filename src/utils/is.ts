ca0c01c05f46ab4762c9d025e3f5fb4d245222fb
shunyue1320 <tsunyue1320@gmail.com>
2023-08-08 13:36:14 +0800

    feat: 全局消息弹窗封装

--- src/utils/is.ts
+++ src/utils/is.ts
@@ -27,3 +27,23 @@ export function isObject(val: any): val is Record<any, any> {
 export function isArray(val: any): val is Array<any> {
   return val && Array.isArray(val)
 }
+
+export function isString(val: unknown): val is string {
+  return is(val, 'String')
+}
+
+export function isEmpty<T = unknown>(val: T): val is T {
+  if (isArray(val) || isString(val)) {
+    return val.length === 0
+  }
+
+  if (val instanceof Map || val instanceof Set) {
+    return val.size === 0
+  }
+
+  if (isObject(val)) {
+    return Object.keys(val).length === 0
+  }
+
+  return false
+}
