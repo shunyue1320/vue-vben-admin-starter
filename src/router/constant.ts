f4b804ac2e4c113ab541abb64fbbafb456ed16ec
shunyue1320 <tsunyue1320@gmail.com>
2023-08-08 22:26:57 +0800

    feat: 添加404页面

--- src/router/constant.ts
+++ src/router/constant.ts
@@ -1,3 +1,18 @@
 export const REDIRECT_NAME = 'Redirect'
 
+export const PARENT_LAYOUT_NAME = 'ParentLayout'
+
+export const PAGE_NOT_FOUND_NAME = 'PageNotFound'
+
+export const EXCEPTION_COMPONENT = () => import('@/views/sys/exception/Exception.vue')
+
 export const LAYOUT = () => import('@/layouts/default/index.vue')
+
+export const getParentLayout = (_name?: string) => {
+  return () =>
+    new Promise((resolve) => {
+      resolve({
+        name: _name || PARENT_LAYOUT_NAME,
+      })
+    })
+}
--- src/router/routes/basic.ts
+++ src/router/routes/basic.ts
@@ -1,6 +1,31 @@
 import type { AppRouteRecordRaw } from '../types'
 
 import { t } from '@/hooks/web/useI18n'
+import { LAYOUT, EXCEPTION_COMPONENT, PAGE_NOT_FOUND_NAME } from '../constant'
+
+// 404 页面
+export const PAGE_NOT_FOUND_ROUTE: AppRouteRecordRaw = {
+  path: '/:path(.*)*',
+  name: PAGE_NOT_FOUND_NAME,
+  component: LAYOUT,
+  meta: {
+    title: 'ErrorPage',
+    hideBreadcrumb: true, // 隐藏面包屑
+    hideMenu: true, // 隐藏菜单
+  },
+  children: [
+    {
+      path: '/:path(.*)*',
+      name: PAGE_NOT_FOUND_NAME,
+      component: EXCEPTION_COMPONENT,
+      meta: {
+        title: 'ErrorPage',
+        hideBreadcrumb: true,
+        hideMenu: true,
+      },
+    },
+  ],
+}
 
 export const ERROR_LOG_ROUTE: AppRouteRecordRaw = {
   path: '/error-log',
