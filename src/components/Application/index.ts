909b6e55308856bc2300779123387e8dc0cb5cd9
shunyue1320 <tsunyue1320@gmail.com>
2023-08-10 10:44:11 +0800

    feat: 封装国际化语言切换组件

--- src/components/Application/index.ts
+++ src/components/Application/index.ts
@@ -1,5 +1,7 @@
 import appProvider from './src/AppProvider.vue'
+import appLocalePicker from './src/AppLocalePicker.vue'
 
 export { useAppProviderContext } from './src/useAppContext'
 
 export const AppProvider = appProvider
+export const AppLocalePicker = appLocalePicker
---
+++ src/components/Application/src/AppLocalePicker.vue
@@ -0,0 +1,72 @@
+<!-- 国际化语言切换组件 -->
+
+<template>
+  <Dropdown
+    placement="bottom"
+    :trigger="['click']"
+    :dropMenuList="localeList"
+    :selectedKeys="selectedKeys"
+    @menu-event="handleMenuEvent"
+    overlayClassName="app-locale-picker-overlay"
+  >
+    <span class="cursor-pointer flex items-center">
+      <Icon icon="ion:language" />
+      <span v-if="showText" class="ml-1">{{ getLocaleText }}</span>
+    </span>
+  </Dropdown>
+</template>
+
+<script lang="ts" setup>
+  import type { LocaleType } from '#/config'
+  import { DropMenu, Dropdown } from '@/components/Dropdown'
+  import { computed, ref, unref, watchEffect } from 'vue'
+  import { useLocale } from '@/locales/useLocale'
+  import { localeList } from '@/settings/localeSetting'
+
+  const props = defineProps({
+    /**
+     * 是否显示文本
+     */
+    showText: { type: Boolean, default: true },
+    /**
+     * 更改时是否刷新界面
+     */
+    reload: { type: Boolean },
+  })
+
+  const selectedKeys = ref<LocaleType[]>([])
+  const { getLocale, changeLocale } = useLocale()
+
+  const getLocaleText = computed(() => {
+    const key = selectedKeys.value[0]
+    if (!key) {
+      return ''
+    }
+    return localeList.find((item) => item.event === key)?.text
+  })
+
+  watchEffect(() => {
+    selectedKeys.value = [unref(getLocale)]
+  })
+
+  async function toggleLocale(lang: LocaleType) {
+    await changeLocale(lang)
+    selectedKeys.value = [lang]
+    props.reload && location.reload()
+  }
+
+  function handleMenuEvent(menu: DropMenu) {
+    if (unref(getLocale) === menu.event) {
+      return
+    }
+    toggleLocale(menu.event as LocaleType)
+  }
+</script>
+
+<style lang="less">
+  .app-locale-picker-overlay {
+    .ant-dropdown-menu-item {
+      min-width: 160px;
+    }
+  }
+</style>
