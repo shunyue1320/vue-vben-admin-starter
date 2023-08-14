d24b945ae949c39b3d2f15fd2fa30e7276b288e3
shunyue1320 <tsunyue1320@gmail.com>
2023-08-14 23:05:26 +0800

    feat: 统一开启关闭监听函数

--- src/utils/domUtils.ts
+++ src/utils/domUtils.ts
@@ -54,3 +54,23 @@ export function removeClass(el: Element, cls: string) {
     el.className = trim(curClass)
   }
 }
+
+export function on(
+  element: Element | HTMLElement | Document | Window,
+  event: string,
+  handler: EventListenerOrEventListenerObject,
+): void {
+  if (element && event && handler) {
+    element.addEventListener(event, handler, false)
+  }
+}
+
+export function off(
+  element: Element | HTMLElement | Document | Window,
+  event: string,
+  handler: Fn,
+): void {
+  if (element && event && handler) {
+    element.removeEventListener(event, handler, false)
+  }
+}
